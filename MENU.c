e0ffdb1d3b639f6d5095d81bf850f38593cbfeec
angelluisarcecarrasco96 <47812243+angelluisarcecarrasco96@users.noreply.github.com>
2019-05-08 10:31:57 +0200

    Update MENU.c

--- MENU.c
+++ MENU.c
@@ -49,3 +49,44 @@ struct Tusuario {
 	char username[20];
 	char password[20];
 };
+void main() {
+
+	//PREMISAS
+	system("color 3F");
+	system("@chcp 1252 > nul");
+
+
+	//ESTRUCTURAS
+	struct Tprecios precios;
+	struct Tcaracteristica casaAAlquilar[NUMCASAS];
+	struct Tfiltros casaBuscada;
+	struct Tusuario miUsuario = { "admin", "1234" };
+
+
+	//VARIABLES
+	char usernameIntroducido[20], passwordIntroducida[20];
+	int i, j, ciudadEncontrada = 0, numDias;
+	float precioFinalPorDia, precioFinalTotal;
+	char opcion, opcion_cancelar, ciudadBuscada[15], c;
+	int casaEncontrada; // Variable para guardar el indice de la casa que cumpla las necesidades del usuario
+	char ciudad[NUMCIUDADES][15] = { "BARCELONA", "VALENCIA", "CADIZ","SANTANDER", "MALAGA", "VIGO", "ALICANTE", "MALLORCA", "GIJON" };
+
+
+
+	//DAMOS VALORES A LAS VARIABLES PRECIOS
+	precios.Piscina = 10;
+	precios.Wifi = 1.5;
+	precios.TipoCasa[0] = 20;
+	precios.TipoCasa[1] = 10;
+	precios.Ubicacion[0] = 15;
+	precios.Ubicacion[1] = 10;
+	precios.Ubicacion[2] = 5;
+	precios.Valoracion[0] = 0.5;
+	precios.Valoracion[1] = 0.75;
+	precios.Valoracion[2] = 1;
+	precios.Valoracion[3] = 1.25;
+	precios.Valoracion[4] = 1.5;
+	precios.MultiplicadorPorTemporada[0] = 1.25;	// Primavera
+	precios.MultiplicadorPorTemporada[1] = 2;		// Verano
+	precios.MultiplicadorPorTemporada[2] = 1;		// Otoño
+	precios.MultiplicadorPorTemporada[3] = 0.75;	// Invierno
