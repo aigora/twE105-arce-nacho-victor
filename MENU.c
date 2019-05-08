0c5579d2317e6e825414de5da24aad2d0e08a861
idorado2019 <48029944+idorado2019@users.noreply.github.com>
2019-05-08 10:23:19 +0200

    Update MENU.c
    declaracion definitiva de estructuras

--- MENU.c
+++ MENU.c
@@ -1,4 +1,5 @@
-#define NUMCASAS 5000  
+#define _CRT_SECURE_NO_WARNINGS
+#define NUMCASAS 10000
 #define NUMCIUDADES 9
 
 #include <stdio.h>
@@ -6,8 +7,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+//NO HAY VARIABLES GLOBALES
+
+
 //ESTRUCTURAS
-struct precios {
+struct Tprecios {
 
 	float Wifi;
 	float Piscina;
@@ -15,22 +19,22 @@ struct precios {
 	float Ubicacion[3];
 	float MultiplicadorPorTemporada[4];
 	float Valoracion[5];
-	};
-	struct caracteristica {
+};
+
+struct Tcaracteristica {
 
 	int tipoDeCasa;
 	int piscina;
 	int ubicacion;
 	int wifi;
 	int numeroDeHabitaciones;
-	int valoracion;      
+	int valoracion;
 	float precioBasePorDia;
 	int indiceCiudad;
 	int alquilada; // 0 = No, 1 = Si
-
 };
 
-struct filtros {
+struct Tfiltros {
 	int temporada;
 	int tipoDeCasa;
 	int piscina;
@@ -40,199 +44,8 @@ struct filtros {
 	int valoracion;
 	char ciudadBuscada[15];//a lo mejor hay que quitar esto porque está definida igual tambien en el main
 };
-struct usuario {
+
+struct Tusuario {
 	char username[20];
-	char password[20]; //estructura usuario para la futura reserva
+	char password[20];
 };
-
-//Declaracion de funciones
-int SeleccionaUbicacion(struct filtros *FiltrosDelUsuario);
-int existeUsuario(struct usuario usuarioValido, char username[], char password[]);
-	
-	
-	
-	
-	
-int main() {
-
-	//ESTRUCTURAS 
-	struct precios NuestrosPreciosPorDia;
-	struct caracteristica casaAAlquilar[NUMCASAS];
-	struct filtros casaBuscada;
-	struct usuario miUsuario = { "admin", "1234" };
-
-
-	//VARIABLES
-
-	
-	char opcion, ciudadBuscada[15], ubicacion;
-	char usernameIntroducido[20], passwordIntroducida[20];
-        int i, j, ciudadEncontrada = 0;
-
-
-
-	
-	
-
-	do { //bucle do-while inicial
-
-			
-			printf("\t\tBIENVENIDOS A NUESTRO PORTAL DE VIVIENDAS\n");
-			printf("\n\t\t\t    -- MENÚ PRINCIPAL DE RASTRIVAGO --\n\n\n\n");
-			printf("\tPara elegir opción, pulse:\n\n");
-			printf("\t  1  -  ALQUILAR.\n\n");
-			printf("\t  2  -  VER CATÁLOGO DE APARTAMENTOS.\n\n");
-			printf("\t  3  -  REGISTRARSE.\n");
-			scanf("%c", &opcion);
-
-			switch (opcion) { //switch para  el menu principal
-			case '1':
-			
-
-				
-				do{
-				
-				
-				printf("Indique que desea: zona cantábrica (C),zona mediterránea (M), zona atlantica(A)\n ");
-				scanf("%c",&ubicacion);
-				
-				
-				switch (ubicacion){ //switch para el menu ubicacion
-					case 'C':
-					case'c':
-							
-							printf("\n\n¿Qué ciudad desea visitar? Elija entre opciones como:\n\n");
-							printf("\tVigo\t\tSantander\n\tGijón\n");
-							scanf(" %s", ciudadBuscada);
-						
-						        ciudadEncontrada = 0;
-			                                for (i = 0; i < NUMCIUDADES; i++) {
-				                        if (strcmp(ciudadBuscada, ciudad[i]) == 0) {
-					                ciudadEncontrada = 1;
-					                break;
-				                         }
-			                                }
-							if (ciudadEncontrada == 1) {
-	
-		
-				                        SeleccionaUbicacion(&casaBuscada);
-				
-						        break;
-								
-					case 'M':
-						case 'm':
-							printf("\n\n¿Qué ciudad desea visitar? Elija entre opciones como:\n\n");
-							printf("\tBarcelona\t\n\tValencia\tMálaga\tAlicante\tMallorca\n");
-							scanf(" %s", ciudadBuscada);
-				
-						        ciudadEncontrada = 0;
-			                                for (i = 0; i < NUMCIUDADES; i++) {
-				                        if (strcmp(ciudadBuscada, ciudad[i]) == 0) {
-					                ciudadEncontrada = 1;
-					                break;
-				                         }
-			                                }
-							
-							break;
-							
-					case 'A':
-						case 'a':
-							
-							printf("\n\n¿Qué ciudad desea visitar? Elija entre opciones como:\n\n");
-							printf("\tCádiz\tHuelva\n\n");
-							scanf(" %s", ciudadBuscada);
-						
-						        ciudadEncontrada = 0;
-			                                for (i = 0; i < NUMCIUDADES; i++) {
-				                        if (strcmp(ciudadBuscada, ciudad[i]) == 0) {
-					                ciudadEncontrada = 1;
-					                break;
-				                         }
-			                                }
-							
-							break;
-							
-				default: printf("La letra pulsada no corresponde a ningun destino\n");
-							
-							
-				}
-				
-				
-				}
-				
-				while(ubicacion!= 'A'||ubicacion!= 'a'||ubicacion!= 'M'||ubicacion!= 'm'||ubicacion!= 'c'||ubicacion!= 'C' );
-				
-				break;				
-				
-				 
-				
-				
-				
-				
-			case '2':
-		
-			
-				
-				printf("\n\t\tBienvenidos a nuestro portal oficial de alquiler de viviendas\n\n");
-				printf("\n\t\t\t    -- Catálogo de apartamentos --\n\n\n");
-				break;
-					
-			case '3':
-
-			for (j = 2; j >= 0; j--) {
-
-				if (existeUsuario(miUsuario, usernameIntroducido, passwordIntroducida) == 1) {
-
-				
-					break;
-				}
-				else {
-					printf("\nEl usuario o la contraseña introducidos no son válidos.\n");
-					printf("Tiene %d intentos restantes\n\n", j);
-				}
-			
-			}
-
-			break;
-
-			
-
-			default:
-				break;
-			}	
-			
-			} while (opcion != '3' && opcion != '3');	
-			
-			
-			
-				}
-//Exposicion de las funciones 
-
-int existeUsuario(struct usuario usuarioValido, char username[], char password[]) {
-
-	printf("\nIntroduzca su usuario: ");
-	scanf("%s", username);
-	printf("\nIntroduzca su contraseña: ");
-	scanf("%s", password);
-
-	if ((strcmp(password, usuarioValido.password) == 0) && (strcmp(username, usuarioValido.username) == 0)) {
-
-		return 1;
-	}
-	else {
-
-		return 0;
-	}
-}
-
-
-int SeleccionaUbicacion(struct filtros *FiltrosDelUsuario) {
-
-	char opcionUbicacion, c;
-
-	do {
-
-		printf("¿Qué ubicación prefiere?:\n\n- Centrico (pulse C)\n\n- Costa (pulse O)\n\n- Alrededores (pulse A)\n\n");
-		scanf("%c", &opcionUbicacion);
-		
-}
